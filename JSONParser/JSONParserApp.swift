cdecbf8aa8bc5d28835a892c564a49a9fe94ffbf
milomiao <wenluma_521@163.com>
2022-09-23 19:20:54 +0800

    Initial Commit

---
+++ JSONParser/JSONParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParserApp.swift
+//  JSONParser
+//
+//  Created by milomiao on 2022/9/23.
+//
+
+import SwiftUI
+
+@main
+struct JSONParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
